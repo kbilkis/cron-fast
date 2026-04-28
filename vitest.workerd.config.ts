78978ce8393445fb3261f1858c9652d443cb2e5f
Kasparas Bilkis <kasparas@bilkis.lt>
2026-04-28 12:45:32 +0300

    remove cli test for workerd

--- vitest.workerd.config.ts
+++ vitest.workerd.config.ts
@@ -2,6 +2,9 @@ import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    exclude: ["test/cli.test.ts"],
+  },
   plugins: [
     cloudflareTest({
       miniflare: {
