5946756b6d9f30b60a0651c544183b786a1a3e66
Kasparas Bilkis <kasparas@bilkis.lt>
2026-04-28 13:00:12 +0300

    fix deno,bun,workerd tests

--- vitest.workerd.config.ts
+++ vitest.workerd.config.ts
@@ -2,9 +2,6 @@ import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    exclude: ["test/cli.test.ts"],
-  },
   plugins: [
     cloudflareTest({
       miniflare: {
